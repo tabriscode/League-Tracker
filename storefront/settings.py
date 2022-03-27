dbdd5f842bdb51727be592b9797ce95df1f3b9fb
tabriscode <michael-540337@hotmail.com>
2022-03-26 22:53:26 -0500

    change css

--- storefront/settings.py
+++ storefront/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 import environ
 
@@ -128,12 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR, "playground/static"
 
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-   BASE_DIR / "playground/static/css/"
+   BASE_DIR / "playground/static/css"
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
